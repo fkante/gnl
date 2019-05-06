cab25271fb90dcd1a4117afbea1505c52e6fc3cc
idinorio laptop <francis.kante@gmail.com>
2019-05-06 22:10:13 +0200

    fonction pour la taille du fichier ok

--- get_next_line.c
+++ get_next_line.c
@@ -1,16 +1,22 @@
-#include "libft.h"
 #include <stdio.h> 
+#include "libft.h"
+
+/*
+** includes for linux needed for Open, Read and Write 
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define BUF_SIZE_1 1
 
-int	ft_size_of_file(const char *file)
+int	ft_size_of_file(int fd)
 {
-	int	fd;
 	int	size_file;
 	char	buf[BUF_SIZE_1 + 1];
 
 	size_file = 0;
-	fd = open(file, O_RDWR);
 	while (read(fd, buf, BUF_SIZE_1))
 		size_file++;
 	printf("size of the file = %d\n", size_file);
@@ -20,5 +26,15 @@ int	ft_size_of_file(const char *file)
 
 int	get_next_line(const int fd, char **line)
 {
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	int fd;
+	if (ac == 1)
+		return (0);
+	fd = open(av[1], O_RDWR);
+	ft_size_of_file(fd);
 	return (0);
 }
