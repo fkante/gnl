3ec25dadcad80779be3f64777f50bbdf0c71418c
Francis KANTE <fkante@e2r5p1.42.fr>
2019-05-09 14:30:17 +0200

    parcours du buffer pour trouver newline

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 10:43:46 by fkante            #+#    #+#             */
-/*   Updated: 2019/05/09 10:59:09 by fkante           ###   ########.fr       */
+/*   Updated: 2019/05/09 14:02:09 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,49 @@
 #include "libft.h"
 
 /*
-** includes for linux needed for Open, Read and Write 
-*/
+ ** includes for linux needed for Open, Read and Write 
+ */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define BUF_SIZE_1 1
+#define BUF_SIZE 20
 
 static int		ft_find_the_newline(int fd)
 {
-	int		index_newline;
-	char	buf[BUF_SIZE_1 + 1];
+	int			index_newline;
+	char		buf[BUF_SIZE + 1];
+	const char	newline = 10;
 
+	int buff_scanner = 0;
 	index_newline = 0;
-	while (read(fd, buf, BUF_SIZE_1))
+	while (read(fd, buf, BUF_SIZE))
 	{
-		index_newline++;
-		if (buf == "\n")
+		while (buff_scanner < BUF_SIZE)
 		{
-			printf("size of the file = %d\n", index_newline);
-			return (0);
+			printf("strcmp: %d\n", ft_strcmp(&buf[buff_scanner] , newline));
+			if (ft_strcmp(&buf[buff_scanner] , "\n") == 0)
+				printf("buf = %c\n", buf[buff_scanner]);
+			buff_scanner++;
 		}
+		index_newline++;
 	}
-	printf("size of the file = %d\n", index_newline);
+	printf("index_newline = %d\n", index_newline);
+	printf("buff_scanner = %d\n", buff_scanner);
 	return (0);
 }
 
 static int		ft_find_the_line(char **line_to_find)
 {
 	static int line_index;
-	
+
 	line_index = 0;
 	printf("pointeur sur char line = %p\n", *line_to_find);
 	while (ft_strcmp(*line_to_find, "\n"))
 	{
 		line_index++;
-		*line_to_find++;
+		//		*line_to_find++;
 	}
 	return (line_index);
 }
@@ -59,6 +64,6 @@ static int		ft_find_the_line(char **line_to_find)
 int		get_next_line(const int fd, char **line)
 {
 	ft_find_the_newline(fd);
-	ft_find_the_line(line);
+	//	ft_find_the_line(line);
 	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 10:47:17 by fkante            #+#    #+#             */
-/*   Updated: 2019/05/09 10:56:52 by fkante           ###   ########.fr       */
+/*   Updated: 2019/05/09 12:42:02 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int		main(int ac, char **av)
 	int fd;
 	char **line;
 
-	line = 0;
+	line = NULL;
 	if (ac == 1)
 		return (0);
 	fd = open(av[1], O_RDWR);
 	get_next_line(fd, line);
-	printf("str_new: %s\n", ft_memjoin_str(av[1], av[2]));
+//	printf("str_new: %s\n", ft_memjoin_str(av[1], av[2]));
 	return (0);
 }
