846958f7adc3c7fc72dae750524e321ca85b60bd
Francis KANTE <fkante@e1r4p23.42.fr>
2019-07-15 16:19:56 +0200

    {leaks} still working on 1 last leak

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,11 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/13 16:18:56 by fkante            #+#    #+#             */
-/*   Updated: 2019/07/15 16:05:00 by fkante           ###   ########.fr       */
+/*   Updated: 2019/07/15 16:10:39 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../srcs/get_next_line.h"
+#include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/uio.h>
@@ -20,7 +20,7 @@ int    main(void)
 {
 	char *line;
 
-	while (get_next_line(1, &line) != 0)
+	while (get_next_line(0, &line) != 0)
 	{
 		printf("%s\n", line);
 	}
